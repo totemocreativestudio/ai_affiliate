07b450d076979a76afe39794b9a7452e08ce1ae4
totemocreativestudio <totemocreativestudio@gmail.com>
2026-09-16 00:40:14 +0700

    Add owner control shell

---
+++ app/components/AdminDashboardV3.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function AdminDashboardV3(){
+  return <section id="administration" className="legacy-page-anchor owner-console"><div className="owner-kicker">LUMAWAY OWNER CONTROL</div><h1>Business Control Center</h1></section>;
+}
