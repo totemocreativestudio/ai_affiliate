71c28fc6f73f0395897790cd0c023a7e080c38ad
totemocreativestudio <totemocreativestudio@gmail.com>
2026-09-16 00:22:46 +0700

    Load owner monitoring styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./luma-v2.css";
 import "./luma-social.css";
 import "./luma-social-extra.css";
 import "./creator360.css";
+import "./owner-monitoring.css";
 import ReferralCapture from "./components/ReferralCapture";
 
 export const metadata = {
