2a456927b17503300505694d61635c1ea8fe4783
totemocreativestudio <totemocreativestudio@gmail.com>
2026-09-20 18:14:28 +0700

    Add branded public Lumaway Insights layout

---
+++ app/web/insights/layout.tsx
@@ -0,0 +1,5 @@
+import "./public-insights.css";
+
+export default function PublicInsightsLayout({children}:{children:React.ReactNode}){
+  return <div className="public-insights-shell">{children}</div>;
+}
